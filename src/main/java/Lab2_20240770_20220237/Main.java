fe4f3e2fd7bf41b86eedd504b275c9560317ee5d
diego <20240770@ricaldone.edu.sv>
2026-06-30 13:37:31 -0600

    primer commit

---
+++ src/main/java/Lab2_20240770_20220237/Main.java
@@ -0,0 +1,9 @@
+package Lab2_20240770_20220237;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Lab2_20240770_20220237/Persona.java
@@ -0,0 +1,5 @@
+package Lab2_20240770_20220237;
+
+//Diego Josué Bonilla Morales 20240770
+public class Persona {
+}
