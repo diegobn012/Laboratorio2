ddb33ae394e7b19f2cba4b72b65ca1b7544084c1
diego <20240770@ricaldone.edu.sv>
2026-06-30 13:48:57 -0600

    Persona

--- src/main/java/Lab2_20240770_20220237/Persona.java
+++ src/main/java/Lab2_20240770_20220237/Persona.java
@@ -1,5 +1,38 @@
 package Lab2_20240770_20220237;
 
 //Diego Josué Bonilla Morales 20240770
-public class Persona {
+public abstract class Persona {
+    private String nombre;
+    private String identificacion;
+    private String correo;
+
+    public Persona(String nombre, String identificacion, String correo) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
